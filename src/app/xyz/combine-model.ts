713f3ab538305541b68db62e6a3c9f8c540a8df6
alan8365 <alan8365@gmail.com>
2020-10-19 00:46:08 +0800

    lint update

--- src/app/xyz/combine-model.ts
+++ src/app/xyz/combine-model.ts
@@ -1,14 +1,14 @@
-import {isCanvas, isVideo} from "./tfjs-model";
+import {isCanvas, isVideo} from './tfjs-model';
 import {
   InputResolution,
   MobileNetMultiplier,
   PoseNetArchitecture,
   PoseNetOutputStride,
   PoseNetQuantBytes
-} from "@tensorflow-models/posenet/dist/types";
-import {drawBoundingBox, drawKeypoints, toggleLoadingUI} from "./util";
+} from '@tensorflow-models/posenet/dist/types';
+import {drawBoundingBox, drawKeypoints, toggleLoadingUI} from './util';
 import Stats from 'stats.js';
-import * as posenet from "@tensorflow-models/posenet";
+import * as posenet from '@tensorflow-models/posenet';
 import clm from 'clmtrackr';
 
 // Setup start
@@ -18,12 +18,12 @@ const videoHeight = 500;
 const stats = new Stats();
 const flipPoseHorizontal = true;
 
-/**
+/*
  net start
  */
 const defaultQuantBytes: PoseNetQuantBytes = 2;
 
-const defaultArchitecture: PoseNetArchitecture = 'MobileNetV1'
+const defaultArchitecture: PoseNetArchitecture = 'MobileNetV1';
 const defaultMobileNetMultiplier: MobileNetMultiplier = 0.75;
 const defaultMobileNetStride: PoseNetOutputStride = 16;
 const defaultMobileNetInputResolution: InputResolution = 500;
@@ -72,8 +72,8 @@ const guiState = {
 };
 
 
-let trackingStarted = false;
-let ctrack = new clm.tracker({
+const trackingStarted = false;
+const ctrack = new clm.tracker({
   faceDetection: {
     useWebWorkers: false,
   },
@@ -84,7 +84,7 @@ ctrack.init();
  * Loads a the camera to be used in the demo
  * @param videoElementId
  */
-export async function setupCamera(videoElementId) {
+export async function setupCamera(videoElementId: any): Promise<HTMLElement> {
   if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
     throw new Error(
       'Browser API navigator.mediaDevices.getUserMedia not available');
@@ -97,8 +97,8 @@ export async function setupCamera(videoElementId) {
     video.height = videoHeight;
 
     video.srcObject = await navigator.mediaDevices.getUserMedia({
-      'audio': false,
-      'video': {
+      audio: false,
+      video: {
         facingMode: 'user',
         width: videoWidth,
         height: videoHeight,
@@ -119,7 +119,7 @@ export async function setupCamera(videoElementId) {
  * Loads a the camera to be used in the demo
  * @param videoElementId
  */
-export async function loadVideo(videoElementId) {
+export async function loadVideo(videoElementId: any): Promise<HTMLElement> {
   const video = await setupCamera(videoElementId);
 
   if (isVideo(video)) {
@@ -134,9 +134,10 @@ export async function loadVideo(videoElementId) {
 /**
  * Bind page with two model.
  * @param videoElementId
- * @param canvasElementId
+ * @param canvasElementIdTFJS
+ * @param canvasElementIdCLM
  */
-export async function bindPage(videoElementId: string, canvasElementIdTFJS: string, canvasElementIdCLM: string) {
+export async function bindPage(videoElementId: string, canvasElementIdTFJS: string, canvasElementIdCLM: string): Promise<void> {
   toggleLoadingUI(true);
 
   const net = await posenet.load({
@@ -150,14 +151,14 @@ export async function bindPage(videoElementId: string, canvasElementIdTFJS: stri
   toggleLoadingUI(false);
 
   let video;
-  let canvasTFJS = document.getElementById(canvasElementIdTFJS);
-  let canvasCLM = document.getElementById(canvasElementIdCLM);
+  const canvasTFJS = document.getElementById(canvasElementIdTFJS);
+  const canvasCLM = document.getElementById(canvasElementIdCLM);
 
   try {
     video = await loadVideo(videoElementId);
   } catch (e) {
     // FIXME general
-    let info = document.getElementById('info');
+    const info = document.getElementById('info');
     info.textContent = 'this browser does not support video capture,' +
       'or this device does not have a camera';
     info.style.display = 'block';
@@ -174,7 +175,7 @@ export async function bindPage(videoElementId: string, canvasElementIdTFJS: stri
  * @param canvasTFJS
  * @param canvasCLM
  */
-export function detect(video, canvasTFJS, canvasCLM) {
+export function detect(video: any, canvasTFJS: any, canvasCLM: any): void {
   if (isCanvas(canvasTFJS) && isCanvas(canvasCLM)) {
     const ctxTFJS = canvasTFJS.getContext('2d');
     const ctxCLM = canvasCLM.getContext('2d');
@@ -189,11 +190,10 @@ export function detect(video, canvasTFJS, canvasCLM) {
     // original image and then just flip the keypoints' x coordinates. If instead
     // we flip the image, then correcting left-right keypoint pairs requires a
     // permutation on all the keypoints.
-    const flipPoseHorizontal = true;
 
-    ctrack.start(video)
+    ctrack.start(video);
 
-    async function poseDetectionFrame() {
+    async function poseDetectionFrame(): Promise<void> {
       // Begin monitoring code for frames per second
       stats.begin();
 
@@ -204,8 +204,8 @@ export function detect(video, canvasTFJS, canvasCLM) {
         decodingMethod: 'single-person'
       });
 
-      let minPoseConfidence = +guiState.singlePoseDetection.minPoseConfidence;
-      let minPartConfidence = +guiState.singlePoseDetection.minPartConfidence;
+      const minPoseConfidence = +guiState.singlePoseDetection.minPoseConfidence;
+      const minPartConfidence = +guiState.singlePoseDetection.minPartConfidence;
 
       // ctxTFJS.clearRect(0, 0, videoWidth, videoHeight);
       //
@@ -222,14 +222,18 @@ export function detect(video, canvasTFJS, canvasCLM) {
           // TODO draw position here
           // drawKeypoints(keypoints, minPartConfidence, ctxTFJS);
           // TODO use position info here
-          parsePosition(keypoints)
+          parsePosition(keypoints);
         }
       });
       // TFJS End
 
       // CLM Start
       ctxCLM.clearRect(0, 0, videoWidth, videoHeight);
-      //psrElement.innerHTML = "score :" + ctrack.getScore().toFixed(4);
+      // psrElement.innerHTML = "score :" + ctrack.getScore().toFixed(4);
+      // ctrack.getCurrentPosition((e) => {
+      //   console.log(e);
+      //   this.draw(canvasCLM);
+      // });
       if (ctrack.getCurrentPosition()) {
         console.log(ctrack);
         ctrack.draw(canvasCLM);
@@ -250,13 +254,13 @@ export function detect(video, canvasTFJS, canvasCLM) {
  * Parse position data
  * @param positions
  */
-function parsePosition(positions) {
+function parsePosition(positions: any): void {
   positions = {
-    'nose': positions[0],
-    'leftEye': positions[1],
-    'rightEye': positions[2],
-    'leftEar': positions[3],
-    'rightEar': positions[4],
+    nose: positions[0],
+    leftEye: positions[1],
+    rightEye: positions[2],
+    leftEar: positions[3],
+    rightEar: positions[4],
   };
 
   // console.log(positions);
